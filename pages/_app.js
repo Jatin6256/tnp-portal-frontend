adc1e46e430388b4e59405ec9093961deb8f0a4e
Aditya Agrawal <adiag1200@gmail.com>
2021-08-03 13:46:19 +0530

    add global loader

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,17 @@ import "../styles/globals.css";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { MuiThemeProvider, createTheme } from "@material-ui/core/styles";
 import Head from "next/head";
+import Router from "next/router";
+import { useState } from "react";
+import { LinearProgress } from "@material-ui/core";
 
 function MyApp({ Component, pageProps }) {
   const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const [loader, setLoader] = useState(false);
+
+  Router.events.on("routeChangeStart", (e) => setLoader(true));
+  Router.events.on("routeChangeComplete", (e) => setLoader(false));
+  Router.events.on("routeChangeError", (e) => setLoader(false));
 
   const theme = createTheme({
     palette: {
@@ -41,12 +49,24 @@ function MyApp({ Component, pageProps }) {
           sizes="any"
           type="logos/svg+xml"
         ></link>
+        <title>TNP IIITR</title>
         <meta
           name="description"
           content="TNP IIIT Portal. One stop destination to manage recruitment process for everyone."
         />
         <link rel="apple-touch-icon" href="/tnp.svg" />
       </Head>
+      {loader && (
+        <LinearProgress
+          style={{
+            position: "absolute",
+            top: 0,
+            width: "100%",
+            zIndex: 2000000,
+          }}
+          color="secondary"
+        />
+      )}
       <Component {...pageProps} />
     </MuiThemeProvider>
   );
